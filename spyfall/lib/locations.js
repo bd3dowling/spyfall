8eabbeeac63ec9beeea7b9c4a58364ed01001de0
BuildTools <unconfigured@null.spigotmc.org>
2016-11-29 22:30:34 +0000

    Fix kek

--- spyfall/lib/locations.js
+++ spyfall/lib/locations.js
@@ -2,337 +2,337 @@ locations = [
   {
     name: 'locations.location1',
     roles: [
-      'locations.roles.location1.a',
-      'locations.roles.location1.b',
-      'locations.roles.location1.c',
-      'locations.roles.location1.d',
-      'locations.roles.location1.e',
-      'locations.roles.location1.f',
-      'locations.roles.location1.g'
+      'locations.roles.location1.1',
+      'locations.roles.location1.2',
+      'locations.roles.location1.3',
+      'locations.roles.location1.4',
+      'locations.roles.location1.5',
+      'locations.roles.location1.6',
+      'locations.roles.location1.7'
     ]
   },
   {
     name: 'locations.location2',
     roles: [
-      'locations.roles.location2.a',
-      'locations.roles.location2.b',
-      'locations.roles.location2.c',
-      'locations.roles.location2.d',
-      'locations.roles.location2.e',
-      'locations.roles.location2.f',
-      'locations.roles.location2.g'
+      'locations.roles.location2.1',
+      'locations.roles.location2.2',
+      'locations.roles.location2.3',
+      'locations.roles.location2.4',
+      'locations.roles.location2.5',
+      'locations.roles.location2.6',
+      'locations.roles.location2.7'
     ]
   },
   {
     name: 'locations.location3',
     roles: [
-      'locations.roles.location3.a',
-      'locations.roles.location3.b',
-      'locations.roles.location3.c',
-      'locations.roles.location3.d',
-      'locations.roles.location3.e',
-      'locations.roles.location3.f',
-      'locations.roles.location3.g'
+      'locations.roles.location3.1',
+      'locations.roles.location3.2',
+      'locations.roles.location3.3',
+      'locations.roles.location3.4',
+      'locations.roles.location3.5',
+      'locations.roles.location3.6',
+      'locations.roles.location3.7'
     ]
   },
   {
     name: 'locations.location4',
     roles: [
-      'locations.roles.location4.a',
-      'locations.roles.location4.b',
-      'locations.roles.location4.c',
-      'locations.roles.location4.d',
-      'locations.roles.location4.e',
-      'locations.roles.location4.f',
-      'locations.roles.location4.g'
+      'locations.roles.location4.1',
+      'locations.roles.location4.2',
+      'locations.roles.location4.3',
+      'locations.roles.location4.4',
+      'locations.roles.location4.5',
+      'locations.roles.location4.6',
+      'locations.roles.location4.7'
     ]
   },
   {
     name: 'locations.location5',
     roles: [
-      'locations.roles.location5.a',
-      'locations.roles.location5.b',
-      'locations.roles.location5.c',
-      'locations.roles.location5.d',
-      'locations.roles.location5.e',
-      'locations.roles.location5.f',
-      'locations.roles.location5.g'
+      'locations.roles.location5.1',
+      'locations.roles.location5.2',
+      'locations.roles.location5.3',
+      'locations.roles.location5.4',
+      'locations.roles.location5.5',
+      'locations.roles.location5.6',
+      'locations.roles.location5.7'
     ]
   },
   {
     name: 'locations.location6',
     roles: [
-      'locations.roles.location6.a',
-      'locations.roles.location6.b',
-      'locations.roles.location6.c',
-      'locations.roles.location6.d',
-      'locations.roles.location6.e',
-      'locations.roles.location6.f',
-      'locations.roles.location6.g'
+      'locations.roles.location6.1',
+      'locations.roles.location6.2',
+      'locations.roles.location6.3',
+      'locations.roles.location6.4',
+      'locations.roles.location6.5',
+      'locations.roles.location6.6',
+      'locations.roles.location6.7'
     ]
   },
   {
     name: 'locations.location7',
     roles: [
-      'locations.roles.location7.a',
-      'locations.roles.location7.b',
-      'locations.roles.location7.c',
-      'locations.roles.location7.d',
-      'locations.roles.location7.e',
-      'locations.roles.location7.f',
-      'locations.roles.location7.g'
+      'locations.roles.location7.1',
+      'locations.roles.location7.2',
+      'locations.roles.location7.3',
+      'locations.roles.location7.4',
+      'locations.roles.location7.5',
+      'locations.roles.location7.6',
+      'locations.roles.location7.7'
     ]
   },
   {
     name: 'locations.location8',
     roles: [
-      'locations.roles.location8.a',
-      'locations.roles.location8.b',
-      'locations.roles.location8.cn',
-      'locations.roles.location8.d',
-      'locations.roles.location8.e',
-      'locations.roles.location8.f',
-      'locations.roles.location8.g'
+      'locations.roles.location8.1',
+      'locations.roles.location8.2',
+      'locations.roles.location8.3',
+      'locations.roles.location8.4',
+      'locations.roles.location8.5',
+      'locations.roles.location8.6',
+      'locations.roles.location8.7'	
     ]
   },
   {
     name: 'locations.location9',
     roles: [
-      'locations.roles.location9.a',
-      'locations.roles.location9.b',
-      'locations.roles.location9.c',
-      'locations.roles.location9.d',
-      'locations.roles.location9.e',
-      'locations.roles.location9.f',
-      'locations.roles.location9.g'
+      'locations.roles.location9.1',
+      'locations.roles.location9.2',
+      'locations.roles.location9.3',
+      'locations.roles.location9.4',
+      'locations.roles.location9.5',
+      'locations.roles.location9.6',
+      'locations.roles.location9.7'
     ]
   },
   {
     name: 'locations.location10',
     roles: [
-      'locations.roles.location10.a',
-      'locations.roles.location10.b',
-      'locations.roles.location10.c',
-      'locations.roles.location10.d',
-      'locations.roles.location10.e',
-      'locations.roles.location10.f',
-      'locations.roles.location10.g'
+      'locations.roles.location10.1',
+      'locations.roles.location10.2',
+      'locations.roles.location10.3',
+      'locations.roles.location10.4',
+      'locations.roles.location10.5',
+      'locations.roles.location10.6',
+      'locations.roles.location10.7'
     ]
   },
   {
     name: 'locations.location11',
     roles: [
-      'locations.roles.location11.a',
-      'locations.roles.location11.b',
-      'locations.roles.location11.c',
-      'locations.roles.location11.d',
-      'locations.roles.location11.e',
-      'locations.roles.location11.f',
-      'locations.roles.location11.g'
+      'locations.roles.location11.1',
+      'locations.roles.location11.2',
+      'locations.roles.location11.3',
+      'locations.roles.location11.4',
+      'locations.roles.location11.5',
+      'locations.roles.location11.6',
+      'locations.roles.location11.7'
     ]
   },
   {
     name: 'locations.location12',
     roles: [
-      'locations.roles.location12.a',
-      'locations.roles.location12.b',
-      'locations.roles.location12.c',
-      'locations.roles.location12.d',
-      'locations.roles.location12.e',
-      'locations.roles.location12.f',
-      'locations.roles.location12.g'
+      'locations.roles.location12.1',
+      'locations.roles.location12.2',
+      'locations.roles.location12.3',
+      'locations.roles.location12.4',
+      'locations.roles.location12.5',
+      'locations.roles.location12.6',
+      'locations.roles.location12.7'
     ]
   },
   {
     name: 'locations.location13',
     roles: [
-      'locations.roles.location13.a',
-      'locations.roles.location13.b',
-      'locations.roles.location13.c',
-      'locations.roles.location13.d',
-      'locations.roles.location13.e',
-      'locations.roles.location13.f',
-      'locations.roles.location13.g'
+      'locations.roles.location13.1',
+      'locations.roles.location13.2',
+      'locations.roles.location13.3',
+      'locations.roles.location13.4',
+      'locations.roles.location13.5',
+      'locations.roles.location13.6',
+      'locations.roles.location13.7'
     ]
   },
   {
     name: 'locations.location14',
     roles: [
-      'locations.roles.location14.a',
-      'locations.roles.location14.b',
-      'locations.roles.location14.c',
-      'locations.roles.location14.d',
-      'locations.roles.location14.e',
-      'locations.roles.location14.f',
-      'locations.roles.location14.g'
+      'locations.roles.location14.1',
+      'locations.roles.location14.2',
+      'locations.roles.location14.3',
+      'locations.roles.location14.4',
+      'locations.roles.location14.5',
+      'locations.roles.location14.6',
+      'locations.roles.location14.7'
     ]
   },
   {
     name: 'locations.location15',
     roles: [
-      'locations.roles.location15.a',
-      'locations.roles.location15.b',
-      'locations.roles.location15.c',
-      'locations.roles.location15.d',
-      'locations.roles.location15.e',
-      'locations.roles.location15.f',
-      'locations.roles.location15.g'
+      'locations.roles.location15.1',
+      'locations.roles.location15.2',
+      'locations.roles.location15.3',
+      'locations.roles.location15.4',
+      'locations.roles.location15.5',
+      'locations.roles.location15.6',
+      'locations.roles.location15.7'
     ]
   },
   {
     name: 'locations.location16',
     roles: [
-      'locations.roles.location16.a',
-      'locations.roles.location16.b',
-      'locations.roles.location16.c',
-      'locations.roles.location16.d',
-      'locations.roles.location16.e',
-      'locations.roles.location16.f',
-      'locations.roles.location16.g'
+      'locations.roles.location16.1',
+      'locations.roles.location16.2',
+      'locations.roles.location16.3',
+      'locations.roles.location16.4',
+      'locations.roles.location16.5',
+      'locations.roles.location16.6',
+      'locations.roles.location16.7'
     ]
   },
   {
     name: 'locations.location17',
     roles: [
-      'locations.roles.location17.a',
-      'locations.roles.location17.b',
-      'locations.roles.location17.c',
-      'locations.roles.location17.d',
-      'locations.roles.location17.e',
-      'locations.roles.location17.f',
-      'locations.roles.location17.g'
+      'locations.roles.location17.1',
+      'locations.roles.location17.2',
+      'locations.roles.location17.3',
+      'locations.roles.location17.4',
+      'locations.roles.location17.5',
+      'locations.roles.location17.6',
+      'locations.roles.location17.7'
     ]
   },
   {
     name: 'locations.location18',
     roles: [
-      'locations.roles.location18.a',
-      'locations.roles.location18.b',
-      'locations.roles.location18.c',
-      'locations.roles.location18.d',
-      'locations.roles.location18.e',
-      'locations.roles.location18.f',
-      'locations.roles.location18.g'
+      'locations.roles.location18.1',
+      'locations.roles.location18.2',
+      'locations.roles.location18.3',
+      'locations.roles.location18.4',
+      'locations.roles.location18.5',
+      'locations.roles.location18.6',
+      'locations.roles.location18.7'
     ]
   },
   {
     name: 'locations.location19',
     roles: [
-      'locations.roles.location19.a',
-      'locations.roles.location19.b',
-      'locations.roles.location19.c',
-      'locations.roles.location19.d',
-      'locations.roles.location19.e',
-      'locations.roles.location19.f',
-      'locations.roles.location19.g'
+      'locations.roles.location19.1',
+      'locations.roles.location19.2',
+      'locations.roles.location19.3',
+      'locations.roles.location19.4',
+      'locations.roles.location19.5',
+      'locations.roles.location19.6',
+      'locations.roles.location19.7'
     ]
   },
   {
     name: 'locations.location20',
     roles: [
-      'locations.roles.location20.a',
-      'locations.roles.location20.b',
-      'locations.roles.location20.c',
-      'locations.roles.location20.d',
-      'locations.roles.location20.e',
-      'locations.roles.location20.f',
-      'locations.roles.location20.g'
+      'locations.roles.location20.1',
+      'locations.roles.location20.2',
+      'locations.roles.location20.3',
+      'locations.roles.location20.4',
+      'locations.roles.location20.5',
+      'locations.roles.location20.6',
+      'locations.roles.location20.7'
     ]
   },
   {
     name: 'locations.location21',
     roles: [
-      'locations.roles.location21.a',
-      'locations.roles.location21.b',
-      'locations.roles.location21.c',
-      'locations.roles.location21.d',
-      'locations.roles.location21.e',
-      'locations.roles.location21.f',
-      'locations.roles.location21.g'
+      'locations.roles.location21.1',
+      'locations.roles.location21.2',
+      'locations.roles.location21.3',
+      'locations.roles.location21.4',
+      'locations.roles.location21.5',
+      'locations.roles.location21.6',
+      'locations.roles.location21.7'
     ]
   },
   {
     name: 'locations.location22',
     roles: [
-      'locations.roles.location22.a',
-      'locations.roles.location22.b',
-      'locations.roles.location22.c',
-      'locations.roles.location22.d',
-      'locations.roles.location22.e',
-      'locations.roles.location22.f',
-      'locations.roles.location22.g'
+      'locations.roles.location22.1',
+      'locations.roles.location22.2',
+      'locations.roles.location22.3',
+      'locations.roles.location22.4',
+      'locations.roles.location22.5',
+      'locations.roles.location22.6',
+      'locations.roles.location22.7'
     ]
   },
   {
     name: 'locations.location23',
     roles: [
-      'locations.roles.location23.a',
-      'locations.roles.location23.b',
-      'locations.roles.location23.c',
-      'locations.roles.location23.d',
-      'locations.roles.location23.e',
-      'locations.roles.location23.f',
-      'locations.roles.location23.g'
+      'locations.roles.location23.1',
+      'locations.roles.location23.2',
+      'locations.roles.location23.3',
+      'locations.roles.location23.4',
+      'locations.roles.location23.5',
+      'locations.roles.location23.6',
+      'locations.roles.location23.7'
     ]
   },
   {
     name: 'locations.location24',
     roles: [
-      'locations.roles.location24.a',
-      'locations.roles.location24.b',
-      'locations.roles.location24.c',
-      'locations.roles.location24.d',
-      'locations.roles.location24.e',
-      'locations.roles.location24.f',
-      'locations.roles.location24.g'
+      'locations.roles.location24.1',
+      'locations.roles.location24.2',
+      'locations.roles.location24.3',
+      'locations.roles.location24.4',
+      'locations.roles.location24.5',
+      'locations.roles.location24.6',
+      'locations.roles.location24.7'
     ]
   },
   {
     name: 'locations.location25',
     roles: [
-      'locations.roles.location25.a',
-      'locations.roles.location25.b',
-      'locations.roles.location25.c',
-      'locations.roles.location25.d',
-      'locations.roles.location25.e',
-      'locations.roles.location25.f',
-      'locations.roles.location25.g'
+      'locations.roles.location25.1',
+      'locations.roles.location25.2',
+      'locations.roles.location25.3',
+      'locations.roles.location25.4',
+      'locations.roles.location25.5',
+      'locations.roles.location25.6',
+      'locations.roles.location25.7'
     ]
   },
   {
     name: 'locations.location26',
     roles: [
-      'locations.roles.location26.a',
-      'locations.roles.location26.b',
-      'locations.roles.location26.c',
-      'locations.roles.location26.d',
-      'locations.roles.location26.e',
-      'locations.roles.location26.f',
-      'locations.roles.location26.g'
+      'locations.roles.location26.1',
+      'locations.roles.location26.2',
+      'locations.roles.location26.3',
+      'locations.roles.location26.4',
+      'locations.roles.location26.5',
+      'locations.roles.location26.6',
+      'locations.roles.location26.7'
     ]
   },
   {
     name: 'locations.location27',
     roles: [
-      'locations.roles.location27.a',
-      'locations.roles.location27.b',
-      'locations.roles.location27.c',
-      'locations.roles.location27.d',
-      'locations.roles.location27.e',
-      'locations.roles.location27.f',
-      'locations.roles.location27.g'
+      'locations.roles.location27.1',
+      'locations.roles.location27.2',
+      'locations.roles.location27.3',
+      'locations.roles.location27.4',
+      'locations.roles.location27.5',
+      'locations.roles.location27.6',
+      'locations.roles.location27.7'
     ]
   },
   {
     name: 'locations.location28',
     roles: [
-      'locations.roles.location28.a',
-      'locations.roles.location28.b',
-      'locations.roles.location28.c',
-      'locations.roles.location28.d',
-      'locations.roles.location28.e',
-      'locations.roles.location28.f',
-      'locations.roles.location28.g'
+      'locations.roles.location28.1',
+      'locations.roles.location28.2',
+      'locations.roles.location28.3',
+      'locations.roles.location28.4',
+      'locations.roles.location28.5',
+      'locations.roles.location28.6',
+      'locations.roles.location28.7'
     ]
   }
 ];
